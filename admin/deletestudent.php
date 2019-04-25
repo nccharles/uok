c7fd3372131d8ef5748b0f4f9e49a90a58c6660e
charles <nccharles1@gmail.com>
2019-04-25 15:48:06 +0200

    changed delete issues

--- admin/deletestudent.php
+++ admin/deletestudent.php
@@ -1,48 +1,43 @@
 <?php
         include("db/dbase.php");
          $id=$_GET['eid'];
-         $sql = $con->prepare("SELECT regnum  FROM student WHERE sid='$id'");
+          $sql = $con->prepare("SELECT seslevid FROM studentsesslevel WHERE sid='$id' and Ystatus='1'");
           $sql->execute();
           $get=$sql->fetchAll();
           foreach($get as $row);
-          $reg=$row['regnum'];
-          $sql = $con->prepare("SELECT levid FROM studentlevel WHERE regnum='$reg' and Ystatus='1'");
-          $sql->execute();
-          $get=$sql->fetchAll();
-          foreach($get as $row);
-          $lev=$row['levid'];
+          $lev=$row['seslevid'];
           if($lev=='1'){
-          $sql = $con->prepare("DELETE  FROM studentlevel WHERE regnum='$reg' and levid='1'");
+          $sql = $con->prepare("DELETE  FROM studentsesslevel WHERE sid='$id' and seslevid='1'");
           $sql->execute();
           $sql = $con->prepare("DELETE  FROM student WHERE sid='$id'");
           $sql->execute(); 
-          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE regnum='$reg' and (levid='1' or levid='0')");
+          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE sid='$id'");
           $sql->execute(); 
-          $sql = $con->prepare("DELETE  FROM marks WHERE regnum='$reg' and level='level 1'");
+          $sql = $con->prepare("DELETE  FROM marks WHERE sid='$id' and level='level 1'");
           $sql->execute();
-          $sql = $con->prepare("DELETE  FROM claim WHERE regnum='$reg' and level='level 1'");
+          $sql = $con->prepare("DELETE  FROM claim WHERE sid='$id' and level='level 1'");
           $sql->execute();
           }else if($lev=='2'){
-           $sql = $con->prepare("DELETE  FROM studentlevel WHERE regnum='$reg' and levid='2'");
+           $sql = $con->prepare("DELETE  FROM studentsesslevel WHERE sid='$id' and seslevid='2'");
           $sql->execute();
-          $sql = $con->prepare("DELETE  FROM marks WHERE regnum='$reg' and level='level 2'");
+          $sql = $con->prepare("DELETE  FROM marks WHERE sid='$id' and level='level 2'");
           $sql->execute();
-          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE regnum='$reg' and levid='2'");
+          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE sid='$id' and seslevid='2'");
           $sql->execute(); 
-          $sql = $con->prepare("UPDATE studentlevel SET Ystatus='1' WHERE regnum='$reg' and levid='1'");
+          $sql = $con->prepare("UPDATE studentsesslevel SET Ystatus='1' WHERE sid='$id' and seslevid='1'");
           $sql->execute(); 
-          $sql = $con->prepare("DELETE  FROM claim WHERE regnum='$reg' and level='level 2'");
+          $sql = $con->prepare("DELETE  FROM claim WHERE sid='$id' and level='level 2'");
           $sql->execute();
           } else{
-          $sql = $con->prepare("DELETE  FROM studentlevel WHERE regnum='$reg' and levid='3'");
+          $sql = $con->prepare("DELETE  FROM studentsesslevel WHERE sid='$id'");
           $sql->execute();
-          $sql = $con->prepare("DELETE  FROM marks WHERE regnum='$reg' and level='level 3'");
+          $sql = $con->prepare("DELETE  FROM marks WHERE sid='$id' and level='level 3'");
           $sql->execute();
-          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE regnum='$reg' and levid='3'");
+          $sql = $con->prepare("DELETE  FROM studentannoucement WHERE sid='$id'");
           $sql->execute(); 
-          $sql = $con->prepare("UPDATE studentlevel SET Ystatus='1' WHERE regnum='$reg' and levid='2'");
+          $sql = $con->prepare("UPDATE studentsesslevel SET Ystatus='1' WHERE sid='$id' and seslevid='2'");
           $sql->execute(); 
-          $sql = $con->prepare("DELETE  FROM claim WHERE regnum='$reg' and level='level 3'");
+          $sql = $con->prepare("DELETE  FROM claim WHERE sid='$id' and level='level 3'");
           $sql->execute();
           }
           header("Location:index.php");
--- admin/studentedit.php
+++ admin/studentedit.php
@@ -14,7 +14,7 @@ $email=$row['email'];
 ?>
 <html>
 <head>
-	<title>Dashboard</title>
+	<title>HOD MIS | DASHBOARD</title>
 	<meta name="viewport" content="width=device-width,initial-scale: 1.0, user-scalabe=0" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
